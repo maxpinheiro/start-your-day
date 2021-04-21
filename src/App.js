c1e668401438f6cca3862bb3f3afe80acc2c242f
Max Pinheiro <maxpinheiro181@gmail.com>
2021-04-21 01:53:04 -0400

    news tab

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import WeatherTab from "./components/WeatherTab";
 import HoroscopeTab from "./components/HoroscopeTab";
+import NewsTab from "./components/NewsTab";
 
 export default class App extends React.Component {
     state = {
@@ -12,14 +13,15 @@ export default class App extends React.Component {
 
     views = ["weather", "horoscope", "news"];
     backgroundClasses = {"weather": "bg-clouds", "horoscope": "bg-stars", "news": "bg-news"};
-    buttonClasses = {"weather": "btn-primary", "horoscope": "btn-secondary", "news": "btn-primary"};
+    buttonClasses = {"weather": "btn-primary", "horoscope": "btn-secondary", "news": "btn-warning"};
     days = {"Mon": "Monday", "Tue": "Tuesday", "Wed": "Wednesday", "Thu": "Thursday", "Fri": "Friday", "Sat": "Saturday", "Sun": "Sunday"};
     months = {"Jan": "January", "Feb": "February", "Mar": "March", "Apr": "April", "May": "May", "Jun": "June", "Jul": "July", "Aug": "August", "Sep": "September", "Oct": "October", "Nov": "November", "Dec": "December"};
     dateSuffix = {"1": "st", "2": "nd", "3": "rd"};
 
     componentDidMount() {
         const [day, month, date, year] = new Date().toDateString().split(' ');
-        this.setState(prevState => ({...prevState, date: `Today is ${this.days[day]}, ${this.months[month]}, ${date}${this.dateSuffix[date%10] || "th"} ${year}`}))
+        this.setState(prevState => ({
+            ...prevState, date: `Today is ${this.days[day]}, ${this.months[month]}, ${date}${this.dateSuffix[date%10] || "th"} ${year}`}))
     }
 
     render() {
@@ -28,7 +30,7 @@ export default class App extends React.Component {
                 <div className="text-white text-center py-3">
                     <h1>Start Your Day</h1>
                     <h3>{this.state.date}</h3>
-                    <div className="btn-group my-2">
+                    <div className="btn-group d-block my-2">
                         {this.views.map(view => (
                             <button className={`btn btn-lg ${this.buttonClasses[this.state.view]} ${this.state.view === view ? 'disabled' : ''}`}
                                     onClick={() => this.setState(prevState => ({...prevState, view, backgroundClass: this.backgroundClasses[view]}))}>
@@ -40,6 +42,7 @@ export default class App extends React.Component {
                 <div className="container-fluid my-4">
                     {this.state.view === "weather" && <WeatherTab />}
                     {this.state.view === "horoscope" && <HoroscopeTab sign={this.state.sign} setSign={(sign) => this.setState(prevState => ({...prevState, sign}))}/>}
+                    {this.state.view === "news" && <NewsTab />}
                 </div>
             </div>
         );
---
+++ src/components/NewsTab.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+export default class NewsTab extends React.Component {
+    state = {
+        articles: [{
+            title: "",
+            url: "",
+            date: ""
+        }]
+    };
+
+
+    componentDidMount() {
+        this.loadNews();
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.sign !== this.props.sign) this.loadHoroscope(this.props.sign || "Capricorn");
+    }
+
+    loadNews() {
+        fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=d889355982654972aed66bb7aa9bf9b4').then(res => res.json().then(data => {
+            this.setState(prevState => (
+                {
+                    articles: data["articles"].slice(0, 10).map(article => {
+                        const date = new Date(article["publishedAt"]);
+                        return {
+                            title: article["title"],
+                            url: article["url"],
+                            date: `${date.getUTCMonth() + 1}/${date.getUTCDate()}/${date.getUTCFullYear()}`
+                        };
+                    }
+                    )
+                }));
+        }));
+    }
+
+    render() {
+        return (
+            <div className="news-container mx-auto text-center text-dark" id="news-content">
+                <h4 className="text-center">Today's Headlines in the US:</h4>
+                <div className="border-top-white pt-3 mx-auto d-block">
+                    {this.state.articles.map(article => (
+                        <div className="">
+                            <a href={article.url || '#'} className="text-dark ">
+                                <p className="headline">{article.title && `${article.title} - ${article.date}`}</p>
+                            </a>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
+    }
+}
